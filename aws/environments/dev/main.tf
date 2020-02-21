a7e2779579e837dc63be74f8210fba24d93f3e5f
John Tambi <jon.tambi@gmail.com>
2020-02-21 16:26:50 -0500

    Adding variables to modules

--- aws/environments/dev/main.tf
+++ aws/environments/dev/main.tf
@@ -5,3 +5,8 @@ provider "aws" {
 module "aws_vpc_dev" {
   source = "../../modules/aws-vpc"
 }
+
+module "aws_eks_cluster_dev" {
+  source = "../../modules/aws-eks"
+  vpc_id = module.aws_vpc_dev.vpc_id
+}
